46388c4588105606a65f331e330e7dd3ca7aa380
Yatin Jain <rokstaryatin2012@gmail.com>
2020-11-23 15:46:05 +0530

    Deployment complete

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -2,8 +2,8 @@ const API_KEY = "76592f12cad9234e1be53581c60446d6";
 
 const requests = {
     fetchNetflixOriginals : `/discover/tv?api_key=${API_KEY}&language=en-US&with_networks=213`,
-    fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-IN`,
-    fetchTopRated : `/movie/top_rated?api_key=${API_KEY}&language=en-IN`,
+    fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies : `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies : `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies : `/discover/movie?api_key=${API_KEY}&with_genres=27`,
